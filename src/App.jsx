651d551bc4d38d05e82b093a4ad96555324cf2f3
Oliver Sitzmann <oliversitzmannr@gmail.com>
2024-10-05 18:13:42 -0400

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import miImagen from '/chapulin_colorado.png'
 function App() {
   return (
     <>
+      <h1 style={styles.title}>Bienvenido a la Liga de Superhéroes</h1>
       <RegistroSuperheroes />
     </>
   )
+}
+
+const styles = {
+  title: {
+    textAlign: 'center',
+    margin: '20px 0',
+    fontFamily: 'Arial, sans-serif',
+    fontSize: '50px',
+    color: '#fff',
+  },
 }
 
 export default App
